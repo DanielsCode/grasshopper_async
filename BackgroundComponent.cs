2f6434b9927fc89e83de1a5dcff3094452774678
DanielsCode <dsiegel@myknack.de>
2017-09-04 16:54:23 +0200

    Initial commit

---
+++ BackgroundComponent.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Grasshopper.Kernel.Attributes;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.GUI;
+using System.Windows.Forms;
+
+namespace ELiSE
+{
+    public class BackgroundComponent : GH_Component
+    {
+
+        private BackgroundForm _optimizationWindow;
+
+
+        /// <summary>
+        /// Initializes a new instance of the BackgroundComponent class.
+        /// </summary>
+        public BackgroundComponent()
+            : base("Background Test", "Background Test",
+                      "Creates a dummy background test",
+                      "Params", "Util")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("e6504823-5b2f-4bab-990f-fa4b8df84491"); }
+        }
+
+        public override void CreateAttributes()
+        {
+            m_attributes = new AttributesA(this);
+        }
+
+        private class AttributesA : GH_ComponentAttributes
+        {
+            public AttributesA(IGH_Component component)
+                : base(component)
+            {
+            }
+
+            public override GH_ObjectResponse RespondToMouseDoubleClick(GH_Canvas sender, GH_CanvasMouseEvent e)
+            {
+                ((BackgroundComponent)Owner).ShowOptimizationWindow();
+                return GH_ObjectResponse.Handled;
+            }
+        }
+
+        public void ShowOptimizationWindow()
+        {
+ 
+            var owner = Grasshopper.Instances.DocumentEditor;
+
+            if (_optimizationWindow == null || _optimizationWindow.IsDisposed)
+            {
+                _optimizationWindow = new BackgroundForm(this) { StartPosition = FormStartPosition.Manual };
+
+                GH_WindowsFormUtil.CenterFormOnWindow(_optimizationWindow, owner, true);
+                owner.FormShepard.RegisterForm(_optimizationWindow);
+
+            }
+            _optimizationWindow.Show(owner);
+        }
+    }
+}
---
+++ BackgroundForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ELiSE
+{
+    partial class BackgroundForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run Simulation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // BackgroundForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 165);
+            this.Controls.Add(this.button1);
+            this.Name = "BackgroundForm";
+            this.Text = "BackgroundForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
